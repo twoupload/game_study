84ce80d405ae9e61956150a0877099ff7bbe0260
SangDo.L <126847724+twoupload@users.noreply.github.com>
2023-06-02 04:20:47 +0900

    Add files via upload

---
+++ tileMap/GameSession.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] int playersLives = 3;
+    [SerializeField] int score = 0;
+
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    void Awake() 
+    {
+        int numGameSession = FindObjectsOfType<GameSession>().Length;
+        if(numGameSession > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start() 
+    {
+        livesText.text = playersLives.ToString();
+        scoreText.text = score.ToString();
+    }
+
+    public void ProcessPlayerDeath()
+    {
+        if(playersLives > 1)
+        {
+            TakeLife();
+        }
+        else
+        {
+            ResetGameSession();
+        }
+    }
+
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
+    }
+
+    void TakeLife()
+    {
+       playersLives--;
+       int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+       SceneManager.LoadScene(currentSceneIndex);
+       livesText.text = playersLives.ToString();
+    }
+
+    void ResetGameSession()
+    {
+        FindObjectOfType<ScenePersist>().ResetScenePersist();
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+    }
+}
